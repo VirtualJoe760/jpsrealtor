09c0b62f6c96317d28c0a61533efcf67c832d29f
Joey Sardella <josephsardella@gmail.com>
2025-01-11 13:26:57 -0800

    fixed success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -22,15 +22,19 @@ export default function SuccessPage() {
 
       {/* Content Section */}
       <div className="flex flex-col items-center justify-center flex-grow px-6 py-20">
-        <div className="text-center max-w-3xl">
+        <div className="max-w-3xl">
           <h2 className="text-4xl font-bold leading-snug mb-6">
             Thank you for reaching out!
           </h2>
           <p className="text-lg leading-relaxed">
             I’m excited to connect with you soon and assist you in finding your dream home here in
-            the stunning Coachella Valley. <strong>In a moment you will recieve a confirmation email for your subscription to the newsletter. Make sure you click the confirmation button.</strong> Together, we’ll make your real estate journey smooth,
+            the stunning Coachella Valley. 
+            </p>
+            <p className="text-xl mt-2 leading-relaxed"><strong>In a moment you will recieve a confirmation email for your subscription to the newsletter. Make sure you click the confirmation button.</strong>
+            </p>
+            <p className="text-lg mt-2 leading-relaxed">Together, we’ll make your real estate journey smooth,
             successful, and truly enjoyable.
-          </p>
+            </p>
           <div className="mt-8">
             <BackButton />
           </div>
